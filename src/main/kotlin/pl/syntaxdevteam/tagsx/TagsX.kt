4422a4d6489a24c947900c3f587073c92bda8c8b
SyntaxDevTeam <wieszczy85@gmail.com>
2025-03-24 10:35:56 +0100

    Przeprojektowana klasa główna wraz z użyciem standardowych klas SyntaxDevTeam

--- src/main/kotlin/pl/syntaxdevteam/tagsx/TagsX.kt
+++ src/main/kotlin/pl/syntaxdevteam/tagsx/TagsX.kt
@@ -1,50 +1,77 @@
 package pl.syntaxdevteam.tagsx
 
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
-import org.bukkit.command.CommandSender
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 import pl.syntaxdevteam.tagsx.commands.CommandManager
+import pl.syntaxdevteam.tagsx.common.*
 import pl.syntaxdevteam.tagsx.data.TagList
 import pl.syntaxdevteam.tagsx.gui.TagsGui
+import pl.syntaxdevteam.tagsx.hooks.HookHandler
 import pl.syntaxdevteam.tagsx.listeners.PlayerJoinListener
 import pl.syntaxdevteam.tagsx.placeholders.TagPlaceholder
 import pl.syntaxdevteam.tagsx.storage.TagStorage
+import java.io.File
+import java.util.*
 
+@Suppress("UnstableApiUsage")
 class TagsX : JavaPlugin() {
-
+    private val config: FileConfiguration = getConfig()
+    var logger: Logger = Logger(this, config.getBoolean("debug"))
+    lateinit var pluginsManager: PluginManager
+    private lateinit var statsCollector: StatsCollector
+    lateinit var messageHandler: MessageHandler
+    private var configHandler = ConfigHandler(this)
+    private lateinit var updateChecker: UpdateChecker
+    private lateinit var hookHandler: HookHandler
+    //private lateinit var commandManager: CommandManager
     val tagStorage = TagStorage(this)
     private val commandManager = CommandManager(this)
 
-    val mm = MiniMessage.miniMessage()
-
-    companion object {
-        val PREFIX = MiniMessage.miniMessage().deserialize("<gradient:#ffcc66:#ff6600>[TagsX]</gradient> ")
-    }
 
     override fun onEnable() {
         saveDefaultConfig()
+        configHandler.verifyAndUpdateConfig()
+        messageHandler = MessageHandler(this).apply { initial() }
+        hookHandler = HookHandler(this)
         tagStorage.loadTags()
         TagList.loadTags(this)
-        commandManager.registerCommands()
 
+        commandManager.registerCommands()
         server.pluginManager.registerEvents(PlayerJoinListener(this), this)
         server.pluginManager.registerEvents(TagsGui(this), this)
 
-        if (server.pluginManager.isPluginEnabled("PlaceholderAPI")) {
+        if (hookHandler.checkPlaceholderAPI()) {
             TagPlaceholder(this).register()
         }
-
-        logger.info("TagsX on")
+        pluginsManager = PluginManager(this)
+        statsCollector = StatsCollector(this)
+        updateChecker = UpdateChecker(this)
+        updateChecker.checkForUpdates()
     }
 
     override fun onDisable() {
         tagStorage.saveTags()
-        logger.info("TagsX off")
+        logger.err("🛑 " + pluginMeta.name + " " + pluginMeta.version + " has been disabled ☹️")
     }
 
-    fun sendMessageWithPrefix(receiver: CommandSender, message: String) {
-        val formattedMessage = LegacyComponentSerializer.legacySection().serialize(PREFIX) + message
-        receiver.sendMessage(formattedMessage)
+    fun getPluginFile(): File {
+        return this.file
+    }
+
+    fun getServerName(): String {
+        val properties = Properties()
+        val file = File("server.properties")
+        if (file.exists()) {
+            properties.load(file.inputStream())
+            val serverName = properties.getProperty("server-name")
+            if (serverName != null) {
+                return serverName
+            } else {
+                logger.debug("Property 'server-name' not found in server.properties file.")
+            }
+        } else {
+            logger.debug("The server.properties file does not exist.")
+        }
+        return "Unknown Server"
     }
 }
