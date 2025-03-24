3c2584b6f3a4e9756b40472d1c1453f3cf2b615e
SyntaxDevTeam <wieszczy85@gmail.com>
2025-03-24 11:18:58 +0100

    Zaktualizowano wersje bibliotek i samego pluginu

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "2.1.20-RC2"
-    id("com.gradleup.shadow") version "9.0.0-beta10"
+    kotlin("jvm") version "2.1.20"
+    id("com.gradleup.shadow") version "9.0.0-beta11"
 }
 
 group = "pl.syntaxdevteam"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-DEV"
 description = "TagsX"
 
 repositories {
@@ -22,10 +22,19 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("org.eclipse.aether:aether-api:1.1.0")
+    compileOnly("org.yaml:snakeyaml:2.4")
+    compileOnly("com.google.code.gson:gson:2.12.1")
     compileOnly("net.kyori:adventure-text-serializer-legacy:4.19.0")
     compileOnly("net.kyori:adventure-text-minimessage:4.19.0")
     compileOnly("net.kyori:adventure-text-serializer-gson:4.19.0")
+    compileOnly("net.kyori:adventure-text-serializer-plain:4.19.0")
+    compileOnly("net.kyori:adventure-text-serializer-ansi:4.19.0")
+    compileOnly("net.luckperms:api:5.4")
     compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:2.3.0")
+    compileOnly("com.github.milkbowl:VaultAPI:1.7.1")
+    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.9")
 }
 
 val targetJavaVersion = 21
