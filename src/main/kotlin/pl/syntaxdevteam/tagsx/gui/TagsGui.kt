20ad0d2653af0855367660165350aa4f72504faf
SyntaxDevTeam <wieszczy85@gmail.com>
2025-03-24 11:16:36 +0100

    Oczyszczono kod, zastąpiono nieaktualne metody Bukkit na nowoczesne Paper, Dodano konfigurowalne treści, usunięto przestarzałe formatowanie, polityka zero warnings

--- src/main/kotlin/pl/syntaxdevteam/tagsx/gui/TagsGui.kt
+++ src/main/kotlin/pl/syntaxdevteam/tagsx/gui/TagsGui.kt
@@ -9,11 +9,10 @@ import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import pl.syntaxdevteam.tagsx.TagsX
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import pl.syntaxdevteam.tagsx.listeners.PlayerJoinListener
 
 class TagsGui(private val plugin: TagsX) : Listener {
-
-    private val legacySerializer = LegacyComponentSerializer.legacySection()
+    private val pjl: PlayerJoinListener = PlayerJoinListener(plugin)
 
     fun open(player: Player) {
         val availableTags = plugin.config.getConfigurationSection("tags")?.getKeys(false)
@@ -23,10 +22,9 @@ class TagsGui(private val plugin: TagsX) : Listener {
             } ?: return
 
         val guiSize = 9 * ((availableTags.size / 9) + 1)
-        val gui: Inventory = Bukkit.createInventory(null, guiSize, "Select Your Tag")
+        val gui: Inventory = Bukkit.createInventory(null, guiSize, plugin.messageHandler.getLogMessage("tags", "title"))
 
         availableTags.forEachIndexed { index, tagKey ->
-            val tagDisplayName = plugin.config.getString("tags.$tagKey.display") ?: "§7[$tagKey]"
             val tagMaterial = Material.matchMaterial(plugin.config.getString("tags.$tagKey.material") ?: "STONE") ?: Material.STONE
 
             gui.setItem(index, createTagItem(tagMaterial, tagKey, player))
@@ -38,14 +36,14 @@ class TagsGui(private val plugin: TagsX) : Listener {
     private fun createTagItem(material: Material, tagKey: String, player: Player): ItemStack {
         val item = ItemStack(material)
         val meta = item.itemMeta?.clone()
+        val tagDisplayName = plugin.config.getString("tags.$tagKey.display") ?: "" //Bo po co masz mieć na czacie jakieś NONE czy tam BRAK
 
-        val tagDisplayName = plugin.config.getString("tags.$tagKey.display") ?: "[None]"
-
-        val fullDisplayName = "§f[$tagDisplayName§f] §r${player.name}"
+        val fullDisplayName = plugin.messageHandler.getLogMessage("tags", "display_tag", mapOf("tag_display_name" to tagDisplayName, "player" to player.name))
 
         if (meta != null) {
-            meta.setDisplayName(fullDisplayName)
-            meta.lore = listOf("§7Click to set this tag")
+            meta.displayName(fullDisplayName)
+
+            meta.lore(listOf(plugin.messageHandler.getLogMessage("tags", "info")))
 
             item.itemMeta = meta
         }
@@ -55,24 +53,23 @@ class TagsGui(private val plugin: TagsX) : Listener {
 
     @EventHandler
     fun onInventoryClick(event: InventoryClickEvent) {
-        if (event.view.title != "Select Your Tag") return
+        if (event.view.title() != plugin.messageHandler.getLogMessage("tags", "title")) return
 
         event.isCancelled = true
         val player = event.whoClicked as Player
         val clickedItem = event.currentItem ?: return
-
         val selectedTag = plugin.config.getConfigurationSection("tags")?.getKeys(false)
             ?.find { tagKey ->
                 val tagMaterial = Material.matchMaterial(plugin.config.getString("tags.$tagKey.material") ?: "STONE")
                 tagMaterial == clickedItem.type
             } ?: return
+        val tagDisplay = plugin.config.getString("tags.$selectedTag.display") ?: ""
 
-        val tagDisplay = plugin.config.getString("tags.$selectedTag.display") ?: "[None]"
         plugin.tagStorage.setTag(player.name, tagDisplay)
         plugin.tagStorage.saveTags()
 
-        plugin.sendMessageWithPrefix(player, "§aYour new tag: §f$tagDisplay")
-
+        player.sendMessage(plugin.messageHandler.getMessage("tags", "new_tag", mapOf("tag_display" to tagDisplay)))
+        pjl.setTag(player) // Upewniam się że tag jest ustawiony bez konieczności relogowania
         player.closeInventory()
     }
 }
