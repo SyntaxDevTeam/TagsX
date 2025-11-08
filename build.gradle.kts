02a088945c592c159c3ae6ff35f08e52418eac87
SyntaxDevTeam <wieszczy85@gmail.com>
2025-11-08 13:02:43 +0100

    The project has been refreshed and all libraries and dependencies have been updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,49 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
-    id("com.gradleup.shadow") version "9.0.0-beta11"
+    kotlin("jvm") version "2.2.21"
+    id("com.gradleup.shadow") version "9.2.2"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("pl.syntaxdevteam.plugindeployer") version "1.0.4"
 }
 
 group = "pl.syntaxdevteam"
-version = "1.0.0-DEV"
+version = "1.0.2"
 description = "TagsX"
 
 repositories {
+    maven("https://nexus.syntaxdevteam.pl/repository/maven-snapshots/") //SyntaxDevTeam
+    maven("https://nexus.syntaxdevteam.pl/repository/maven-releases/") //SyntaxDevTeam
+    gradlePluginPortal()
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "papermc-repo"
+        name = "papermc"
     }
     maven("https://oss.sonatype.org/content/groups/public/") {
         name = "sonatype"
     }
-    maven("https://repo.extendedclip.com/releases/")
-    maven("https://jitpack.io")
-    maven("https://repo.codemc.org/repository/maven-public")
+    maven("https://repo.extendedclip.com/releases/") // PlaceholderAPI
+    maven("https://repo.codemc.org/repository/maven-public/") // VaultUnlockedAPI
+    maven("https://jitpack.io") // VaultAPI
+    maven("https://repo.essentialsx.net/releases/") // EssentialsX
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly("pl.syntaxdevteam:core:1.2.5-SNAPSHOT")
+    compileOnly("pl.syntaxdevteam:messageHandler:1.0.3")
+    compileOnly("pl.syntaxdevteam:cleanerx:1.5.3")
     compileOnly("org.eclipse.aether:aether-api:1.1.0")
-    compileOnly("org.yaml:snakeyaml:2.4")
-    compileOnly("com.google.code.gson:gson:2.12.1")
-    compileOnly("net.kyori:adventure-text-serializer-legacy:4.19.0")
-    compileOnly("net.kyori:adventure-text-minimessage:4.19.0")
-    compileOnly("net.kyori:adventure-text-serializer-gson:4.19.0")
-    compileOnly("net.kyori:adventure-text-serializer-plain:4.19.0")
-    compileOnly("net.kyori:adventure-text-serializer-ansi:4.19.0")
-    compileOnly("net.luckperms:api:5.4")
-    compileOnly("me.clip:placeholderapi:2.11.6")
-    compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:2.3.0")
+    compileOnly("org.yaml:snakeyaml:2.5")
+    compileOnly("com.google.code.gson:gson:2.13.2")
+    compileOnly("net.kyori:adventure-text-serializer-legacy:4.25.0")
+    compileOnly("net.kyori:adventure-text-minimessage:4.25.0")
+    compileOnly("net.kyori:adventure-text-serializer-gson:4.25.0")
+    compileOnly("net.kyori:adventure-text-serializer-plain:4.25.0")
+    compileOnly("net.kyori:adventure-text-serializer-ansi:4.25.0")
+    compileOnly("net.luckperms:api:5.5")
+    compileOnly("me.clip:placeholderapi:2.11.7")
+    compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:3.1.0")
     compileOnly("com.github.milkbowl:VaultAPI:1.7.1")
-    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.9")
+    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.15")
 }
 
 val targetJavaVersion = 21
@@ -42,15 +51,37 @@ kotlin {
     jvmToolchain(targetJavaVersion)
 }
 
-tasks.build {
-    dependsOn("shadowJar")
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    runServer {
+        minecraftVersion("1.21.10")
+        runDirectory(file("run/paper"))
+    }
+    runPaper.folia.registerTask()
 }
 
 tasks.processResources {
-    val props = mapOf("version" to version, "description" to description)
+    val props = mapOf(
+        "version" to version,
+        "description" to description
+    )
     inputs.properties(props)
     filteringCharset = "UTF-8"
-    filesMatching("paper-plugin.yml") {
+    filesMatching(listOf("paper-plugin.yml")) {
         expand(props)
     }
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set("PunisherX")
+    archiveClassifier.set("")
+    archiveVersion.set(project.version.toString())
+    mergeServiceFiles()
+}
+
+plugindeployer {
+    paper { dir = "/home/debian/poligon/Paper/1.21.10/plugins" } //ostatnia wersja dla Paper
+    folia { dir = "/home/debian/poligon/Folia/1.21.8/plugins" } //ostatnia wersja dla Folia
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "TagsX"
+pluginManagement {
+    repositories {
+        // Twoje repozytoria z pluginami:
+        maven("https://nexus.syntaxdevteam.pl/repository/maven-releases/")
+        maven("https://nexus.syntaxdevteam.pl/repository/maven-snapshots/")
+
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
